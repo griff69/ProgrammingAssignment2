dcecf83b26f39e2df3b927389e3e5c5961715ff9
griff6978 <egriffin33@gatech.edu>
2015-05-23 00:52:57 -0700

    Assignment 2 makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions compute the matrix inverse storing it in cache so that 
+## next reference to the  matrix inverse will get it from cache rather than
+## re computing the lavel again
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
+##########################################################################
+##      AUthor: Eric H. Griffin 
+##  Assignment: #2 
+##    Function: makeCacheMatrix
+## Description: This function creates a special "matrix" object 
+##              that can cache it inverse
+##        Date: 5/21/15
+##########################################################################
+
+makeCacheMatrix <- function(matx = matrix()) 
+                     {
+	
+	              invMatx <- NULL 
+	              setMatx <- function(maty)
+                                    {
+		                      matx <<- maty
+		                      invMatx <<- NULL
+	                            }
+	
+	              getMatx <- function() matx
+	              setInvMatx <- function(inverseMatx) invMatx <<- inverseMatx
+	              getInvMatx <- function() invMatx
+	
+	              list(setMatx = setMatx, getMatx=getMatx,
+		                     setInvMatx = setInvMatx,
+		                     getInvMatx = getInvMatx)
+                     }
 
 
 ## Write a short comment describing this function
